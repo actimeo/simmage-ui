28cac50eb873bce435a69386b85c0efe79a1fabd
pmartin <contact@elol.fr>
2016-08-24 15:30:38 +0200

    [layout] Admin center layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MdSidenavModule } from '@angular2-material/sidenav/sidenav';
+import { MdButtonModule } from '@angular2-material/button/button';
+import { MdToolbarModule } from '@angular2-material/toolbar/toolbar';
+import { MdIconModule } from '@angular2-material/icon/icon';
 
 import { routing, appRoutingProviders } from './app.routing';
 
@@ -13,6 +17,7 @@ import { UsersComponent } from './admin/users/users.component';
 
 import { AppComponent } from './app.component';
 import { MainCenterComponent } from './main/main-center/main-center.component';
+import { SidenavComponent } from './admin/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +27,18 @@ import { MainCenterComponent } from './main/main-center/main-center.component';
     TopicsComponent,
     OrgansComponent,
     UsergroupsComponent,
-    UsersComponent
+    UsersComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
-    routing
+    routing,
+    MdSidenavModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [appRoutingProviders],
   entryComponents: [AppComponent],
