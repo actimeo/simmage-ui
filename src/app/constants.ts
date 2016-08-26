5a0d36a26e9440532bc1953f6e5fcd6f060df7c9
pmartin <contact@elol.fr>
2016-08-26 14:52:02 +0200

    Comment on constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,10 +1,10 @@
 export class Constants {
-  static KEY_AUTH_TOKEN = 'auth_token';
-  static KEY_AUTH_RIGHTS = 'auth_rights';
-  static KEY_AUTH_UGR_ID = 'auth_ugr_id';
-  static KEY_AUTH_FIRSTNAME = 'auth_firstname';
-  static KEY_AUTH_LASTNAME = 'auth_lastname';
+  static KEY_AUTH_TOKEN = 'auth_token'; // auth token to be used when calling pg procedures
+  static KEY_AUTH_RIGHTS = 'auth_rights'; // admin rights of the current user
+  static KEY_AUTH_UGR_ID = 'auth_ugr_id'; // usergroup id of the user 
+  static KEY_AUTH_FIRSTNAME = 'auth_firstname'; // user firstname
+  static KEY_AUTH_LASTNAME = 'auth_lastname'; // user lastname
 
-  static KEY_SEL_POR_ID = 'sel_por_id';
-  static KEY_SEL_GRP_ID = 'sel_grp_id';
+  static KEY_SEL_POR_ID = 'sel_por_id'; // currenctly selected portal id, 0 if none
+  static KEY_SEL_GRP_ID = 'sel_grp_id'; // currently selected group id, 0 if none
 }
