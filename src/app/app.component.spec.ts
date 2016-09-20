da027576e5f809797f1115e9f4737ea3a2cf0fd8
pmartin <contact@elol.fr>
2016-09-20 10:21:56 +0200

    (tests) AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,102 @@
 /* tslint:disable:no-unused-variable */
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+
 import { AppComponent } from './app.component';
+import { UserService, UserData } from './db-services/user.service';
+
+let comp: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
+
+const fakeUserService = {
+  userDataState: new Subject<UserData>()
+};
+
+class RouterStub {
+  navigate(path: Array<string>) { return path; }
+}
+
 
 describe('App: SimmageUi', () => {
-  beforeEach(() => {
-//    addProviders([AppComponent]);
-  });
-/*
-  it('should create the app',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app).toBeTruthy();
-    }));
 
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
-    }));*/
+  beforeEach(() => {
+  });
+
+  it('should subscribe/unsubscribe', () => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: UserService, useValue: fakeUserService },
+        { provide: Router, useClass: RouterStub }
+      ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+    comp.ngOnInit();
+    fixture.detectChanges();
+    expect(comp.sub).not.toBeNull('subscription should not be null');
+    expect(comp.sub).toEqual(jasmine.any(Subscription), 'should be a subscription');
+
+    spyOn(comp.sub, 'unsubscribe');
+    comp.ngOnDestroy();
+    expect(comp.sub.unsubscribe).toHaveBeenCalled();
+  });
+
+  it('should go to login page when logged out', () => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: UserService, useValue: fakeUserService },
+        { provide: Router, useClass: RouterStub }
+      ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    spyOn(comp.router, 'navigate');
+
+    fixture.detectChanges();
+
+    comp.ngOnInit();
+    let userData: UserData = new UserData(null);
+    userData.loggedIn = false;
+
+    fakeUserService.userDataState.next(userData);
+    fixture.detectChanges();
+    expect(comp.router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should go nowhere when logged in', () => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: UserService, useValue: fakeUserService },
+        { provide: Router, useClass: RouterStub }
+      ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    spyOn(comp.router, 'navigate');
+
+    fixture.detectChanges();
+
+    comp.ngOnInit();
+    let userData: UserData = new UserData(null);
+    userData.loggedIn = true;
+
+    fakeUserService.userDataState.next(userData);
+    fixture.detectChanges();
+    expect(comp.router.navigate).not.toHaveBeenCalled();
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import './rxjs_operators';
 
 import { UserService, UserData } from './db-services/user.service';
@@ -10,17 +11,29 @@ import { UserService, UserData } from './db-services/user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor(private user: UserService, private router: Router) {
+  public sub: Subscription = null;
+
+  constructor(private user: UserService, public router: Router) {
+  }
+
+  ngOnInit() {
     let source: Observable<boolean> = this.user.userDataState.map((u: UserData) => u.loggedIn);
 
     // Go to home at login
-//    source.filter((u: boolean) => u === true)
-//      .subscribe(() => this.router.navigate(['/']));
+    //    source.filter((u: boolean) => u === true)
+    //      .subscribe(() => this.router.navigate(['/']));
 
     // Go to login page when logged out
-    source.filter((u: boolean) => u === false)
+    this.sub = source
+      .filter((u: boolean) => u === false)
       .subscribe(() => this.router.navigate(['/login']));
+  }
+
+  ngOnDestroy() {
+    if (this.sub !== null) {
+      this.sub.unsubscribe();
+    }
   }
 }
