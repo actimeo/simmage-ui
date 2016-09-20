b32e73cb233912a1622fed3dd73101918813f8a5
pmartin <contact@elol.fr>
2016-09-20 20:23:47 +0200

    (tests) ErrorMsg component

--- src/app/common/error-msg/error-msg.component.spec.ts
+++ src/app/common/error-msg/error-msg.component.spec.ts
@@ -1,11 +1,44 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 import { ErrorMsgComponent } from './error-msg.component';
+import { AppModule } from '../../app.module';
+
+let fixture: ComponentFixture<ErrorMsgComponent>;
+let component: ErrorMsgComponent;
+let els: DebugElement[];
+let el: DebugElement;
 
 describe('Component: ErrorMsg', () => {
   it('should create an instance', () => {
-    let component = new ErrorMsgComponent();
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+      providers: []
+    });
+
+    fixture = TestBed.createComponent(ErrorMsgComponent);
+    component = fixture.componentInstance;
     expect(component).toBeTruthy();
+
+    component.msg = '';
+    fixture.detectChanges();
+    els = fixture.debugElement.queryAll(By.css('div.app-error'));
+    expect(els.length).toEqual(0, 'message should not appear');
+
+    component.msg = 'a non empty message';
+    fixture.detectChanges();
+    els = fixture.debugElement.queryAll(By.css('div.app-error'));
+    expect(els.length).toEqual(1, 'message should appear');
+
+    const element = fixture.nativeElement;
+    const iconToggle = element.querySelector('md-icon');
+    iconToggle.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    expect(component.detailsVisible).toEqual(true);
+    els = fixture.debugElement.queryAll(By.css('p.pre'));
+    expect(els.length).toEqual(1, 'details should appear');
   });
 });
