7a0b00f590a2dfc8687d59489b5bbdb822255bc5
Jeremy Beaupuy <jeremy.beaupuy@epsi.fr>
2016-09-20 13:37:21 +0200

    Organ Service tests

--- src/app/db-services/organ.service.spec.ts
+++ src/app/db-services/organ.service.spec.ts
@@ -2,18 +2,99 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { OrganService } from './organ.service';
-import { UserService } from './user.service';
+import { Observable } from 'rxjs/Observable';
 import { PgService } from '../pg.service';
+import { UserService } from './user.service';
 
+class FakeUserService {
+  public userData: any = { token: 123456789 };
+}
 
 describe('Service: Organ', () => {
-/*  beforeEach(() => {
+
+  const fakePgService = jasmine.createSpyObj('PgService', ['pgcall']);
+  const fakeUserService = new FakeUserService();
+  const userToken = 123456789;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [OrganService, UserService, PgService]
+      providers: [
+        OrganService,
+        { provide: UserService, useValue: fakeUserService },
+        { provide: PgService, useValue: fakePgService }
+      ]
     });
   });
 
-  /*it('should ...', inject([OrganService, UserService, PgService], (service: OrganService) => {
+  it('should ...', inject([OrganService], (service: OrganService) => {
     expect(service).toBeTruthy();
-  }));*/
+  }));
+
+  it('should return an Observable of an organization', inject([OrganService], (service: OrganService) => {
+    const organ = {
+      org_id: 1,
+      org_name: 'Organization',
+      org_description: 'Description',
+      org_internal: true
+    };
+
+    const organId = 1;
+
+    fakePgService.pgcall.and.returnValue(Observable.of(organ));
+
+    service.loadOrgan(organId).subscribe(() => {
+      expect(fakePgService.pgcall)
+      .toHaveBeenCalledWith('organ/organization_get', {
+        prm_token: userToken,
+        prm_id: organId
+      });
+    });
+   }));
+
+   it('should return a boolean when we update an organization', inject([OrganService], (service: OrganService) => {
+     fakePgService.pgcall.and.returnValue(Observable.of(true));
+
+     const organId = 1;
+     const organName = 'Organization';
+     const organDescription = 'Description';
+
+     service.updateOrgan(organId, organName, organDescription, true).subscribe(() => {
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('organ/organization_set', {
+         prm_token: userToken,
+         prm_id: organId,
+         prm_name: organName,
+         prm_description: organDescription,
+         prm_internal: true
+       });
+     });
+   }));
+
+   it('should return a number which is the id of the new organization', inject([OrganService], (service: OrganService) => {
+     fakePgService.pgcall.and.returnValue(Observable.of(1));
+
+     const organName = 'Organization';
+     const organDescription = 'Description';
+
+     service.addOrgan(organName, organDescription, false).subscribe(() => {
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('organ/organization_add', {
+         prm_token: userToken,
+         prm_name: organName,
+         prm_description: organDescription,
+         prm_internal: false
+       });
+     });
+   }));
+
+   it('should return a boolean when deleting an organization', inject([OrganService], (service: OrganService) => {
+     fakePgService.pgcall.and.returnValue(Observable.of(true));
+
+     const organId = 1;
+
+     service.deleteOrgan(organId).subscribe(() => {
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('organ/organization_delete', {
+          prm_token: userToken,
+          prm_id: organId
+       });
+     });
+   }));
 });
