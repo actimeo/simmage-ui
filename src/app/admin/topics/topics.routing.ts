f624d620bfdd379bf97b2957548467628bbeb36b
pmartin <contact@elol.fr>
2016-09-21 16:19:43 +0200

    debug topics module

--- src/app/admin/topics/topics.routing.ts
+++ src/app/admin/topics/topics.routing.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { TopicsComponent } from './topics-center/topics.component';
 import { TopicComponent } from './topic/topic.component';
 import { TopicsListComponent } from './topics-list/topics-list.component';
-import { Test1Component } from './test1/test1.component';
 
 import { TopicResolve } from '../topic-resolve.guard';
 import { CanDeactivateGuard } from '../../guards/can-deactivate.guard';
@@ -13,7 +12,6 @@ export const topicsRoutes: Routes = [
     path: '', component: TopicsComponent,
     children: [
       { path: '', pathMatch: 'full', component: TopicsListComponent },
-      { path: 'test', component: Test1Component },
       { path: 'new', component: TopicComponent },
       {
         path: ':id', component: TopicComponent,
