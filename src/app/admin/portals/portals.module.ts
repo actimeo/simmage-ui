e4090fc7bd5c95dff10cefbdb47ee21a55ed8e80
pmartin <contact@elol.fr>
2016-09-27 14:20:45 +0200

    (admin) portals module

--- src/app/admin/portals/portals.module.ts
+++ src/app/admin/portals/portals.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PortalsComponent } from './portals-center/portals.component';
 
+import { SharedModule } from '../../shared/shared.module';
+
+import { PortalsComponent } from './portals-center/portals.component';
 import { portalsRouting } from './portals.routing';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule.forRoot(),
     portalsRouting,
   ],
   declarations: [PortalsComponent]
