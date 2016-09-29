26df36c8eff7517bffd2c9d872b8873af7c0b001
Jeremy Beaupuy <jeremy.beaupuy@epsi.fr>
2016-09-29 14:24:05 +0200

    Group add/delete/update/edit self done

--- src/app/admin/groups/group.service.ts
+++ src/app/admin/groups/group.service.ts
@@ -1,15 +1,66 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import '../../rxjs_operators';
 
 import { UserService } from '../../shared/user.service';
 import { PgService } from '../../pg.service';
+import { DbGroup, DbOrganization } from '../../db-models/organ';
 
 @Injectable()
-export class GroupService implements OnDestroy {
+export class GroupService {
 
   constructor(private user: UserService, private pg: PgService) { }
 
-  
+  loadGroups(): Observable<DbGroup[]> {
+    return this.pg.pgcall('organ/group_list', {
+      prm_token: this.user.userData.token,
+      prm_org_id: null,
+      prm_internal: null
+    });
+  }
+
+  addGroup(name: string, description: string, mandatory: boolean, orientation: string, organization: number): Observable<number> {
+    return this.pg.pgcall('organ/group_add', {
+      prm_token: this.user.userData.token,
+      prm_name: name,
+      prm_description: description,
+      prm_mandatory: mandatory,
+      prm_orientation: orientation,
+      prm_org_id: organization
+    });
+  }
+
+  updateGroup(id: number, name: string, description: string, mandatory: boolean, orientation: string, organization: number): Observable<boolean> {
+    return this.pg.pgcall('organ/group_update', {
+      prm_token: this.user.userData.token,
+      prm_id: id,
+      prm_name: name,
+      prm_description: description,
+      prm_mandatory: mandatory,
+      prm_orientation: orientation,
+      prm_org_id: organization
+    });
+  }
+
+  deleteGroup(id: number) {
+    return this.pg.pgcall('organ/group_delete', {
+      prm_token: this.user.userData.token,
+      prm_id: id
+    });
+  }
+
+  getGroup(id: number): Observable<DbGroup> {
+    return this.pg.pgcall('organ/group_get', {
+      prm_token: this.user.userData.token,
+      prm_id: id
+    });
+  }
+
+  loadOrganizations(): Observable<DbOrganization[]> {
+    return this.pg.pgcall('organ/organization_list', {
+      prm_token: this.user.userData.token,
+      prm_internal: null
+    });
+  }
 
 }
--- src/app/admin/groups/group/group.component.ts
+++ src/app/admin/groups/group/group.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 import { GroupService } from '../group.service';
-import { DbGroup } from '../../../db-models/organ';
+import { DbGroup, DbOrganization } from '../../../db-models/organ';
 import { CanComponentDeactivate } from '../../../guards/can-deactivate.guard';
 
 @Component({
@@ -16,8 +16,14 @@ export class GroupComponent implements OnInit, CanComponentDeactivate {
   id: number;
   creatingNew: boolean = false;
 
+  organizationList: DbOrganization[];
+
   form: FormGroup;
   nameCtrl: FormControl;
+  descriptionCtrl: FormControl;
+  mandatoryCtrl: FormControl;
+  orientationCtrl: FormControl;
+  organizationCtrl: FormControl;
 
   originalData: DbGroup = { 
     grp_id: null,
@@ -36,7 +42,117 @@ export class GroupComponent implements OnInit, CanComponentDeactivate {
     private fb: FormBuilder, private gs: GroupService) { }
 
   ngOnInit() {
+    this.nameCtrl = new FormControl('', Validators.required);
+    this.descriptionCtrl = new FormControl('', Validators.required);
+    this.mandatoryCtrl = new FormControl('', Validators.required);
+    this.orientationCtrl = new FormControl('', Validators.required);
+    this.organizationCtrl = new FormControl('', Validators.required);
+    this.form = this.fb.group({
+      name: this.nameCtrl,
+      description: this.descriptionCtrl,
+      mandatory: this.mandatoryCtrl,
+      orientation: this.orientationCtrl,
+      organization: this.organizationCtrl
+    });
 
+    this.gs.loadOrganizations().subscribe(organs => this.organizationList = organs);
+
+    this.route.data.forEach((data: { group: DbGroup }) => {
+      if ('group' in data) {
+        this.id = data.group.grp_id;
+        this.creatingNew = false;
+        this.nameCtrl.setValue(data.group.grp_name);
+        this.descriptionCtrl.setValue(data.group.grp_description);
+        this.mandatoryCtrl.setValue(data.group.grp_mandatory);
+        this.orientationCtrl.setValue(data.group.grp_orientation);
+        this.organizationCtrl.setValue(data.group.org_id);
+      } else {
+        this.creatingNew = true;
+        this.nameCtrl.setValue('');
+        this.descriptionCtrl.setValue('');
+        this.mandatoryCtrl.setValue('');
+        this.orientationCtrl.setValue('');
+        this.organizationCtrl.setValue('');
+      }
+      this.setOriginalDataFromFields();
+      this.errorMsg = '';
+      this.errorDetails = '';
+    });
+  }
+
+  onSubmit() {
+    this.setOriginalDataFromFields();
+    if (this.creatingNew) {
+      this.gs.addGroup(this.nameCtrl.value, this.descriptionCtrl.value,
+        this.mandatoryCtrl.value, this.orientationCtrl.value, this.organizationCtrl.value)
+        .subscribe((ret: number) => {
+          this.id = ret;
+          this.goBackToList(true);
+        },
+        (err) => {
+          this.errorMsg = 'Error occured while adding a group';
+          this.errorDetails = err.text();
+        });
+    } else {
+      this.gs.updateGroup(this.id, this.nameCtrl.value, this.descriptionCtrl.value,
+        this.mandatoryCtrl.value, this.orientationCtrl.value, this.organizationCtrl.value)
+        .subscribe(ret => {
+          this.goBackToList(true);
+        },
+        (err) => {
+          this.errorMsg = 'Error while updating a group';
+          this.errorDetails = err.text();
+        });
+    }
+  }
+
+  doCancel() {
+    this.setOriginalDataFromFields();
+    this.goBackToList();
+  }
+
+  doDelete() {
+    this.setOriginalDataFromFields();
+    this.gs.deleteGroup(this.id).subscribe(ret => {
+      this.goBackToList();
+    },
+    (err) => {
+      this.errorMsg = 'Error while deleting a group';
+      this.errorDetails = err.text();
+    });
+  }
+
+  private goBackToList(withSelected = false) {
+    if (withSelected) {
+      this.router.navigate(['/admin/groups', { selid: this.id }]);
+    } else {
+      this.router.navigate(['/admin/groups']);
+    }
+  }
+
+  canDeactivate() {
+    if (this.originalDataChanged()) {
+      this.pleaseSave = true;
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  private setOriginalDataFromFields() {
+    this.originalData.grp_name = this.nameCtrl.value;
+    this.originalData.grp_description = this.descriptionCtrl.value;
+    this.originalData.grp_mandatory = this.mandatoryCtrl.value;
+    this.originalData.grp_orientation = this.orientationCtrl.value;
+    this.originalData.org_id = this.organizationCtrl.value;
+  }
+
+  private originalDataChanged() {
+    return this.originalData.grp_name !== this.nameCtrl.value
+    || this.originalData.grp_description !== this.descriptionCtrl.value
+    || this.originalData.grp_mandatory !== this.mandatoryCtrl.value
+    || this.originalData.grp_orientation !== this.orientationCtrl.value
+    || this.originalData.org_id !== this.organizationCtrl.value;
   }
 
 }
