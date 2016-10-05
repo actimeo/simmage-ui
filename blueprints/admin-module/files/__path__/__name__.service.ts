601d91547408d89be22a0171ddea1875bf818d92
Jeremy Beaupuy <jeremy.beaupuy@epsi.fr>
2016-10-05 16:49:10 +0200

    Blueprint - added generic tests + fixed function names

--- blueprints/admin-module/files/__path__/__name__.service.ts
+++ blueprints/admin-module/files/__path__/__name__.service.ts
@@ -1,41 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { UserService } from '../../shared/user.service';
-import { PgService } from '../../pg.service';
+// import { UserService } from '../../shared/user.service';
+// import { PgService } from '../../pg.service';
 
 
 @Injectable()
 export class <%= classifiedModuleName %>Service {
 
-  constructor() { }
+  private data<%= classifiedModuleName %>: any[] = [{
+    id: 1,
+    name: 'a name'
+  },
+  {
+    id: 2,
+    name: 'another name'
+  }];
 
-  private data: any[] = [{
-      id: 1,
-      name: 'a name'
-    },
-    {
-      id: 2,
-      name: 'another name'
-    }];
+  constructor() { }  
 
-  public load(id: number): Observable<any> {
-    return Observable.of(this.data[id-1]);
+  public get<%= classifiedModuleName %>(id: number): Observable<any> {
+    return Observable.of(this.data<%= classifiedModuleName %>[id - 1]);
   }
 
-  public update(id: number, name: string): Observable<boolean> {
+  public update<%= classifiedModuleName %>(id: number, name: string): Observable<boolean> {
     return Observable.of(true);
   }
 
-  public add(name: string): Observable<number> {
+  public add<%= classifiedModuleName %>(name: string): Observable<number> {
     return Observable.of(2);
   }
 
-  public delete(id: number) {
+  public delete<%= classifiedModuleName %>(id: number) {
     return Observable.of(true);
   }
 
-  public loadAll(): Observable<any[]> {
-    return Observable.of(this.data);
+  public load<%= classifiedModuleName %>(): Observable<any[]> {
+    return Observable.of(this.data<%= classifiedModuleName %>);
   }
 }
