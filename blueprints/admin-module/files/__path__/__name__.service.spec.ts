70d29a815967d77f27d7102a8f2bd929f44507e7
Jeremy Beaupuy <jeremy.beaupuy@epsi.fr>
2016-10-06 09:16:22 +0200

    Updated blueprint service tests

--- blueprints/admin-module/files/__path__/__name__.service.spec.ts
+++ blueprints/admin-module/files/__path__/__name__.service.spec.ts
@@ -10,11 +10,23 @@ class FakeUserService {
   public userData: any = { token: 123456789 };
 }
 
+// TODO : replace all "schema" occurences by the correct emplacement
+
 describe('Service: <%= classifiedModuleName %>', () => {
 
   const fakePgService = jasmine.createSpyObj('PgService', ['pgcall']);
   const fakeUserService = new FakeUserService();
   const userToken = 123456789;
+  const data = [
+    {
+      id: 1,
+      name: 'a name'
+    },
+    {
+      id: 3,
+      name: 'another name'
+    }
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -28,56 +40,72 @@ describe('Service: <%= classifiedModuleName %>', () => {
 
   it('should return an Observable of type <%= dasherizedModuleName %>', inject([<%= classifiedModuleName %>Service], (service: <%= classifiedModuleName %>Service) => {
     const <%= camelizedModuleName %> = {
-      // Todo : fill content of mock <%= classifiedModuleName %> object with an id of 1
       id: 1,
       name: 'a name'
     };
 
     const <%= camelizedModuleName %>Id = 1;
 
-    //fakePgService.pgcall.and.returnValue(Observable.of(<%= camelizedModuleName %>));
+    fakePgService.pgcall.and.returnValue(Observable.of(<%= camelizedModuleName %>));
 
     service.get<%= classifiedModuleName %>(<%= camelizedModuleName %>Id).subscribe(obj => {
-      expect(obj).toEqual(<%= camelizedModuleName %>);
+      expect(fakePgService.pgcall).toHaveBeenCalledWith('"schema"/<%= camelizedModuleName %>_get', {
+        prm_token: userToken,
+        prm_id: 1
+      });
     });
   }));
 
   it('should return a number which is the id of the new <%= dasherizedModuleName %>', inject([<%= classifiedModuleName %>Service], (service: <%= classifiedModuleName %>Service) => {
-     const <%= camelizedModuleName %>Name = '<%= classifiedModuleName %>';
+     const <%= camelizedModuleName %>Name = 'a name';
      // TODO : declare all other fields of <%= camelizedModuleName %> object
 
+     fakePgService.pgcall.and.returnValue(Observable.of(1));
+
      service.add<%= classifiedModuleName %>(<%= camelizedModuleName %>Name /* TODO : fill with other necessary arguments */).subscribe(obs => {
-       expect(obs).toBe(2);
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('"schema"/<%= camelizedModuleName %>_add', {
+        prm_token: userToken,
+        prm_name: 'a name'
+      });
      });
    }));
 
    it('should return a boolean when we update <%= dasherizedModuleName %> object', inject([<%= classifiedModuleName %>Service], (service: <%= classifiedModuleName %>Service) => {
-     //fakePgService.pgcall.and.returnValue(Observable.of(true));
+     fakePgService.pgcall.and.returnValue(Observable.of(true));
 
      const <%= camelizedModuleName %>Id = 1;
      const <%= camelizedModuleName %>Name = '<%= classifiedModuleName %>';
      // TODO : declare all other fields of <%= camelizedModuleName %> object
 
      service.update<%= classifiedModuleName %>(<%= camelizedModuleName %>Id, <%= camelizedModuleName %>Name /* TODO : fill with other necessary arguments */).subscribe(obs => {
-       expect(obs).toBe(true);
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('"schema"/<%= camelizedModuleName %>_update', {
+        prm_token: userToken,
+        prm_id: 1,
+        prm_name: 'a name'
+      });
      });
    }));
 
    it('should return a boolean when deleting <%= dasherizedModuleName %> object', inject([<%= classifiedModuleName %>Service], (service: <%= classifiedModuleName %>Service) => {
-     //fakePgService.pgcall.and.returnValue(Observable.of(true));
+     fakePgService.pgcall.and.returnValue(Observable.of(true));
 
      const <%= camelizedModuleName %>Id = 1;
 
      service.delete<%= classifiedModuleName %>(<%= camelizedModuleName %>Id).subscribe(obs => {
-       expect(obs).toBe(true);
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('"schema"/<%= camelizedModuleName %>_delete', {
+        prm_token: userToken,
+        prm_id: 1
+      });
      });
    }));
 
    it('should return a list of 2 <%= dasherizedModuleName %> objects by default', inject([<%= classifiedModuleName %>Service], (service: <%= classifiedModuleName %>Service) => {
-     //fakePgService.pgcall.and.returnValue(Observable.of(true));
+     fakePgService.pgcall.and.returnValue(Observable.of(data));
 
      service.load<%= classifiedModuleName %>().subscribe(obs => {
-       expect(obs.length).toBe(2);
+       expect(fakePgService.pgcall).toHaveBeenCalledWith('"schema"/<%= camelizedModuleName %>_list', {
+        prm_token: userToken
+      });
      });
    }));
 });
