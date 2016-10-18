4b20fb45d6d909a9857b79d9f5cb932176d2daa3
pmartin <contact@elol.fr>
2016-10-18 17:48:53 +0200

    (buleprint) add resolver to router config

--- blueprints/admin-module/files/__path__/__name__.routing.ts
+++ blueprints/admin-module/files/__path__/__name__.routing.ts
@@ -5,16 +5,25 @@ import { <%= classifiedModuleName %>CenterComponent } from './<%= dasherizedModu
 import { <%= classifiedModuleName %>ListComponent } from './<%= dasherizedModuleName %>-list/<%= dasherizedModuleName %>-list.component';
 import { <%= classifiedModuleName %>FormComponent } from './<%= dasherizedModuleName %>-form/<%= dasherizedModuleName %>-form.component';
 import { <%= classifiedModuleName %>Resolve } from './<%= dasherizedModuleName %>-resolve.guard';
+import { <%= classifiedModuleName %>ListResolve } from './<%= dasherizedModuleName %>-list-resolve.guard';
 
 export const <%= camelizedModuleName %>Routes: Routes = [
   {
     path: '', component: <%= classifiedModuleName %>CenterComponent, children: [
-      { path: '', component: <%= classifiedModuleName %>ListComponent }
+      {
+        path: '',
+        component: <%= classifiedModuleName %>ListComponent,
+        resolve: { list: <%= classifiedModuleName %>ListResolve }
+      }
     ]
   },
   {
     path: 'new', component: <%= classifiedModuleName %>CenterComponent, children: [
-      { path: '', component: <%= classifiedModuleName %>ListComponent },
+      {
+        path: '',
+        component: <%= classifiedModuleName %>ListComponent,
+        resolve: { list: <%= classifiedModuleName %>ListResolve }
+      },
       {
         path: '',
         component: <%= classifiedModuleName %>FormComponent,
@@ -25,15 +34,19 @@ export const <%= camelizedModuleName %>Routes: Routes = [
   },
   {
     path: ':id', component: <%= classifiedModuleName %>CenterComponent, children: [
-      { path: '', component: <%= classifiedModuleName %>ListComponent },
+      {
+        path: '',
+        component: <%= classifiedModuleName %>ListComponent,
+        resolve: { list: <%= classifiedModuleName %>ListResolve }
+      },
       {
         path: '',
         component: <%= classifiedModuleName %>FormComponent,
         resolve: { <%= camelizedModuleName %>: <%= classifiedModuleName %>Resolve },
-        canDeactivate: [CanDeactivateGuard],
-        outlet: 'details'
+  canDeactivate: [CanDeactivateGuard],
+  outlet: 'details'
       }
-    ]
+]
   }
 ];
 
