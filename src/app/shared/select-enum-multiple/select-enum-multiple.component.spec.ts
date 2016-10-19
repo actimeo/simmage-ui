c799ea5a96faa7c53c93fd654936f7af9323aaeb
pmartin <contact@elol.fr>
2016-10-19 11:18:34 +0200

    (shared) select enum multiple form control

---
+++ src/app/shared/select-enum-multiple/select-enum-multiple.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SelectEnumMultipleComponent } from './select-enum-multiple.component';
+
+describe('Component: SelectEnumMultiple', () => {
+  it('should create an instance', () => {
+    let component = new SelectEnumMultipleComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/select-enum-multiple/select-enum-multiple.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+
+import { EnumsService } from '../enums.service';
+
+@Component({
+  selector: 'app-select-enum-multiple',
+  templateUrl: './select-enum-multiple.component.html',
+  styleUrls: ['./select-enum-multiple.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectEnumMultipleComponent),
+      multi: true
+    }
+  ]
+
+})
+export class SelectEnumMultipleComponent implements OnInit, ControlValueAccessor {
+
+  @Input() dbenum;
+  private value: string[] = [];
+  private valuesList: Observable<string[]>;
+
+  private propagateChange = (_: any) => { };
+
+  constructor(private enums: EnumsService) { }
+
+  ngOnInit() {
+    this.valuesList = this.enums.enum_list(this.dbenum);
+  }
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched() { }
+
+  onCheckboxChange(ev) {
+    this.value = this.value.filter(t => t !== ev.target.value);
+    if (ev.target.checked) {
+      this.value.push(ev.target.value);
+    }
+    this.propagateChange(this.value);
+  }
+}
