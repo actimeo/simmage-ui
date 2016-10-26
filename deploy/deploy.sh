4b71f4943152df2277e90c11172b61940d2f5509
pmartin <contact@elol.fr>
2016-10-26 22:35:33 +0200

    (ci) test create key on deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+openssl aes-256-cbc -K $encrypted_b52bebe2eeaf_key -iv $encrypted_b52bebe2eeaf_iv -in deploy_rsa.enc -out deploy_rsa -d
 
 rsync -r --delete dist/ "${DEPLOY_USER}@${DEPLOY_HOST}:simmage-ui-dist"
 ssh deploy@maya.elol.fr ./deploy-ui.sh
