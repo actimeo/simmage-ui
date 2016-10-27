673521fe38df564efb07c90c79cfef71632e41ef
pmartin <contact@elol.fr>
2016-10-27 11:47:34 +0200

    Refactor usergroup list

--- src/app/rxjs_operators.ts
+++ src/app/rxjs_operators.ts
@@ -1,5 +1,6 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/reduce';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/share';
@@ -10,6 +11,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/pluck';
 import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/toArray';
 
 import 'rxjs/add/observable/zip';
 import 'rxjs/add/observable/from';
