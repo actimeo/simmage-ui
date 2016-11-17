5e7b5b98cc11de6535ae7a03e4368042fbc299bc
pmartin <contact@elol.fr>
2016-11-17 16:24:46 +0100

    (i18n) disable translations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
+/*
 import { TRANSLATION_DE } from './i18n/messages.de';
 import { TRANSLATION_EN } from './i18n/messages.en';
 import { TRANSLATION_FR } from './i18n/messages.fr';
@@ -14,19 +15,19 @@ const translation_map = {
   'fr': TRANSLATION_FR
 };
 const langCode = window.localStorage.getItem('lang');
-
+*/
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule,
   {
-/*    providers: [
-      {
+    providers: [
+/*      {
         provide: TRANSLATIONS,
         useValue: langCode in translation_map ? translation_map[langCode] : TRANSLATION_EN
       },
       { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
-      { provide: LOCALE_ID, useValue: langCode }
-    ]*/
+      { provide: LOCALE_ID, useValue: langCode }*/
+    ]
   });
