ba26e1bb7353a7d03ff11d58250855e8358d41d8
pmartin <contact@elol.fr>
2016-11-18 15:30:27 +0100

    i18n enable

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
-/*
+
 import { TRANSLATION_DE } from './i18n/messages.de';
 import { TRANSLATION_EN } from './i18n/messages.en';
 import { TRANSLATION_FR } from './i18n/messages.fr';
@@ -15,7 +15,7 @@ const translation_map = {
   'fr': TRANSLATION_FR
 };
 const langCode = window.localStorage.getItem('lang');
-*/
+
 if (environment.production) {
   enableProdMode();
 }
@@ -23,11 +23,11 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule,
   {
     providers: [
-/*      {
+      {
         provide: TRANSLATIONS,
         useValue: langCode in translation_map ? translation_map[langCode] : TRANSLATION_EN
       },
       { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
-      { provide: LOCALE_ID, useValue: langCode }*/
+      { provide: LOCALE_ID, useValue: langCode }
     ]
   });
