e620bd20b2f28e760b41f5b3bab9f5e7d48c1ff3
pmartin <contact@elol.fr>
2017-01-12 12:16:42 +0100

    main documents refactor

--- src/app/main/pages/documents/documents-center/documents-center.component.ts
+++ src/app/main/pages/documents/documents-center/documents-center.component.ts
@@ -1,42 +1,23 @@
 import { DocumentsService } from './../../../../shared/documents.service';
-import { Component, OnInit, OnChanges, OnDestroy, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
 import { DbMainmenu } from './../../../../db-models/portal';
-import { DbTopic } from './../../../../db-models/organ';
 
 @Component({
   selector: 'app-documents-center',
   templateUrl: './documents-center.component.html',
   styleUrls: ['./documents-center.component.css']
 })
-export class DocumentsCenterComponent implements OnInit, OnChanges, OnDestroy {
+export class DocumentsCenterComponent implements OnInit {
 
-  public viewTopics: Observable<DbTopic[]>;
-  private idSub: Subscription;
   public mainmenu: Observable<DbMainmenu>;
-  private contentId: number;
 
   constructor(public documentsService: DocumentsService, private r: ActivatedRoute) { }
 
   ngOnInit() {
     this.mainmenu = this.r.data.pluck('data');
-    this.idSub = this.r.data.pluck('data')
-      .distinctUntilChanged().subscribe((data: DbMainmenu) => {
-        this.contentId = data.mme_content_id;
-        this.viewTopics = this.documentsService.loadViewTopics(this.contentId);
-      });
-  }
-
-  public ngOnChanges(changes: SimpleChanges) {
-    this.viewTopics = this.documentsService.loadViewTopics(this.contentId);
-  }
-
-  ngOnDestroy() {
-    if (this.idSub) {
-      this.idSub.unsubscribe();
-    }
   }
 }
