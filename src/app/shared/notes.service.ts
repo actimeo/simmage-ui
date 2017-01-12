0a55cc3659cc4c128480cb038e31ee4a03c92a8a
Jeremy Beaupuy <jbeaupuy@actimeo.fr>
2017-01-12 15:41:23 +0100

    Show all notes created by and sent to current user in my account notes page

--- src/app/shared/notes.service.ts
+++ src/app/shared/notes.service.ts
@@ -36,4 +36,39 @@ export class NotesService {
   public loadViewTopics(nov_id: number): Observable<any[]> {
     return this.pg.pgcall('notes/notesview_get_topics', { prm_id: nov_id });
   }
+
+  public loadNotesForUser(): Observable<NoteJson[]> {
+    let req = {
+      not_id: true,
+      not_text: true,
+      not_object: true,
+      not_event_date: true,
+      not_creation_date: true,
+      author: {
+        par_id: true,
+        par_lastname: true,
+        par_firstname: true
+      },
+      topics: {
+        top_id: true,
+        top_name: true,
+        top_icon: true,
+        top_color: true
+      },
+      dossiers: {
+        dos_id: true,
+        dos_firstname: true,
+        dos_lastname: true
+      },
+      recipients: {
+        par_id: true,
+        par_lastname: true,
+        par_firstname: true,
+        nor_for_action: true
+      }
+    };
+    return this.pg.pgcall('notes/note_participant_list', {
+      req: JSON.stringify(req)
+    });
+  }
 }
